0b73bbbc703639b74deac6f693ee7738c2c4b4fe
Pooja s <poojasengamalai27@gmail.com>
2026-05-25 19:20:44 +0530

    Treasure Hunter

--- NewspaperAgency.java
+++ NewspaperAgency.java
@@ -5,7 +5,7 @@ public class NewspaperAgency {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        int Profit = ((a*b)-(a*c))-100;
+        int Profit = (a*b)-(a*c)-100;
         System.out.println(Profit);
         sc.close();
     }
