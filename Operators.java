afc48dcf55a31313b73dc3d0a190a23c9b279bef
Pooja s <poojasengamalai27@gmail.com>
2026-05-25 12:33:21 +0530

    Operators

--- Operators.java
+++ Operators.java
@@ -2,7 +2,7 @@ public class Operators {
     public static void main(String[] args) {
         int a = 10;
         float b = 20f;
-        double c = 5.7;
+        double c = 7.4;
         double x = a * b + c;
         System.out.println(x);
         
