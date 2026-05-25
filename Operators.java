81c31d73437c3454936645be45ddf80a6d1c67c3
Pooja s <poojasengamalai27@gmail.com>
2026-05-25 12:15:56 +0530

    Operators

---
+++ Operators.java
@@ -0,0 +1,11 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 20f;
+        double c = 5.7;
+        double x = a * b + c;
+        System.out.println(x);
+        
+
+    }
+}
