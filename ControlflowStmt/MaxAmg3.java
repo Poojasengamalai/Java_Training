3bb5a73f34d8f7a8e27d26a4acfce22844e52d60
Pooja s <poojasengamalai27@gmail.com>
2026-05-26 12:11:11 +0530

    Send maximum among 3 numbers

---
+++ ControlflowStmt/MaxAmg3.java
@@ -0,0 +1,19 @@
+package ControlflowStmt;
+
+import java.util.*;
+public class MaxAmg3 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if(a>=b && a<=c || a>=c && a<=b){
+            System.out.println(a);
+        }else if(b>=a && b<=c || b<=a && b>=c){
+            System.out.println(b);
+        }else if(c>=a && c<=b || c<=a && c>=b){
+            System.out.println(c);
+        }
+        sc.close();
+    }
+}
