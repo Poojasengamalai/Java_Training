1898452ad86d8d2be1fcc6938dd4864257963e85
Pooja s <poojasengamalai27@gmail.com>
2026-05-27 13:58:22 +0530

    Pyramit using star

--- PatternConcept/Pyramid.java
+++ PatternConcept/Pyramid.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Pyramid {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i=1;i<=n;i++){          //for rows
-            for(int j=1;j<=n;j++){      //for colums and after splitting triangle it both are equal then j<=n if a column is reduced then j<=n-1
-                if((i+j)>=(n+1)){
+        int num = sc.nextInt();
+        for(int i=1;i<=num;i++){          //for rows
+            for(int j=1;j<=num;j++){      //for colums and after splitting triangle it both are equal then j<=n if a column is reduced then j<=n-1
+                if((i+j)>=(num+1)){
                 System.out.print("* ");
                 }else{
                     System.out.print("  ");
