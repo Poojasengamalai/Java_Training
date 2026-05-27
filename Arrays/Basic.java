3e0d8e01bd137037312f48fe5ea96573303e6e9b
Pooja s <poojasengamalai27@gmail.com>
2026-05-27 14:40:00 +0530

    Basic array diclaration Initialization and Assign values

--- Arrays/Basic.java
+++ Arrays/Basic.java
@@ -2,8 +2,8 @@ package Arrays;
 
 public class Basic {
     public static void main(String args[]){
-        // int []arr = new int[0];
-        // System.out.print(arr);  // gives allocated memory
+        int []arr = new int[0];
+        System.out.print(arr);  // gives allocated memory
         // System.out.print(arr[]); //may occur array index out of bound exception like without index values
         int []arr1 = {1,3,5,7};
         System.out.println(arr1[3]);
