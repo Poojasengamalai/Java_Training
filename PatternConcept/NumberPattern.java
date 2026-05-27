53832d2332c5cace20542cf67966b29a19e4033b
Pooja s <poojasengamalai27@gmail.com>
2026-05-27 14:12:04 +0530

    Number pattern

--- PatternConcept/NumberPattern.java
+++ PatternConcept/NumberPattern.java
@@ -1,13 +1,14 @@
 package PatternConcept;
 
 import java.util.Scanner;
-public class StartPattern {
+public class NumberPattern {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print("* ");
+        // int a = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print(j +" "); // print j then same numbers in columns, print i then same number in rows 
             }
             System.out.println();
         }
