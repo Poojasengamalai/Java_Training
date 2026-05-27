9ceaadd283261d41a3b86eb0be1d49bd665ed556
Pooja s <poojasengamalai27@gmail.com>
2026-05-27 12:19:02 +0530

    Pyramid pattern using star

---
+++ PatternConcept/Pyramid.java
@@ -0,0 +1,25 @@
+package PatternConcept;
+
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){          //for rows
+            for(int j=1;j<=n;j++){      //for colums and after splitting triangle it both are equal then j<=n if a column is reduced then j<=n-1
+                if((i+j)>=(n+1)){
+                System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+                for(int k=1;k<=i;k++){      //for mirror triangle at a time or else for reducing one column in the end then k<i
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            sc.close();
+        }
+    }
+
