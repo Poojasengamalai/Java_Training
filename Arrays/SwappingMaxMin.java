deec25f260a93e2ac9ff228e1c9097c6cfcb4e9b
Pooja s <poojasengamalai27@gmail.com>
2026-05-28 12:40:07 +0530

    Swapping of Numbers based of maximum and minimum

---
+++ Arrays/SwappingMaxMin.java
@@ -0,0 +1,56 @@
+package Arrays;
+
+import java.util.*;
+public class SwappingMaxMin {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        // int min = 0;
+        // int max = n-1;
+
+
+        // for(int i=0;i<n;i++){
+        //     if(i%2==0){
+        //         System.out.print(arr[max]+" ");
+        //         max--;
+        //     }else{
+        //         System.out.print(arr[min]+" ");
+        //         min++;
+        //     }
+        // }
+
+
+        // while(min<=max){
+        //     if(min==max){
+        //         System.out.print(arr[min]);
+        //     }else{
+        //         System.out.print(arr[max]+" "+arr[min]+" ");
+        //     }
+        //         max--;
+        //         min++;
+        // }
+
+
+        if(n%2==0){
+            for(int i=0; i<n/2; i++){
+                    System.out.print(arr[n-(i+1)]+" "+arr[i]+ " ");
+            }
+        }else{
+            for(int i=0; i<(n/2)+1; i++){
+                if(i==n/2){
+                    System.out.print(arr[i]);
+                }else{
+                    System.out.print(arr[n-(i+1)]+" "+arr[i]+ " ");
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
