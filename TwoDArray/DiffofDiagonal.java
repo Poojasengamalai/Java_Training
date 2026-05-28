c1b403df0001eb912a79add4d5fadd2e330c24e3
Pooja s <poojasengamalai27@gmail.com>
2026-05-28 16:36:53 +0530

    Finding difference of diagonals of an array

--- TwoDArray/DiffofDiagonal.java
+++ TwoDArray/DiffofDiagonal.java
@@ -7,15 +7,23 @@ public class DiffofDiagonal {
         int n = sc.nextInt();
         int arr[][] = new int[n][n];
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                arr[i][j] = sc.nextInt();
+            for(int k=0;k<n;k++){
+                arr[i][k] = sc.nextInt();
             }
         }
-        int left =0;
+        int left = 0;
         int right = 0;
         for(int i =0;i<n;i++){
             left+=arr[i][i];
             right+=arr[i][n-i-1];
+        }
+        int diff = 0;
+        if(left>right){
+            diff = left-right;
+            System.out.println(diff);
+        }else{
+            diff = right-left;
+            System.out.println(diff);
         }
         System.out.println((left>right) ? left-right : right-left);
         sc.close();
