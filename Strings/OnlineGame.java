a643c7f373bab8356d59487a250671ee7b57b5f5
Pooja s <poojasengamalai27@gmail.com>
2026-05-29 10:03:42 +0530

    Printing the all odd numbers after the all even numbers with space

--- Strings/OnlineGame.java
+++ Strings/OnlineGame.java
@@ -9,19 +9,38 @@ public class OnlineGame {
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-        String game = "";
-        for(int i=0;i<n;i++){
-            if(arr[i]%2==0){
-                game = game + arr[i]+" ";
+        // String game = "";
+
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]%2==0){
+        //         game = game + arr[i]+" ";
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]%2==1){
+        //         game = game + arr[i]+" ";
+        //     }
+        // }
+        int left = 0;
+        int right = n-1;
+        while(left < right){
+            while(arr[left]%2==0){
+                left++;
+            }
+            while(arr[right]%2==1){
+                right--;
+            }
+            if(left < right){
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
             }
         }
-        for(int i=0;i<n;i++){
-            if(arr[i]%2==1){
-                game = game + arr[i]+" ";
-            }
-        }
+
         System.out.println("Array After Segregation");
-        System.out.println(game);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
         sc.close();
     }    
 }
