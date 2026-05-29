9346f918221e5e33344ce73db4f111a135a64f46
Pooja s <poojasengamalai27@gmail.com>
2026-05-29 20:52:55 +0530

    Basic Printin Statements in Java

---
+++ BasicsJava/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello MKCE!!!!");//curser in new line after printing
+        System.out.print("Hello");//curser in same line after printing
+        System.out.printf("Hiiii");
+    }
+}
