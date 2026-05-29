354a88b24261ae2a9737f9a0b7bc4981e50cc33e
Pooja s <poojasengamalai27@gmail.com>
2026-05-29 20:46:23 +0530

    Basic Printing Statements in java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello MKCE!!!!");//curser in new line after printing
-        System.out.print("Hello");//curser in same line after printing
-        System.out.printf("Hiiii");
-    }
-}
