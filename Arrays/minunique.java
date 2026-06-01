06e9469349874c1da1ac9a254049d975a7b13b05
Pooja s <poojasengamalai27@gmail.com>
2026-06-01 15:15:39 +0530

    Finding minimum unique element of an array

---
+++ Arrays/minunique.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.*;
+public class minunique {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int count[] = new int[127];
+        for(int i=0;i<n;i++){
+            count[arr[i]-0]++;
+        }
+        for(int i=0;i<n;i++){
+        if(count[i]==1){
+            System.out.println(i);
+            break;
+        }
+    }
+    sc.close();
+    }
+}
