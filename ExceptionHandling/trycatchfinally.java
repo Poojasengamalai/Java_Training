ead769bc6bb8f08c85dc39a1b600601fe51c5c83
Pooja s <poojasengamalai27@gmail.com>
2026-06-01 15:57:29 +0530

    Exception Handling using try catch finally

--- ExceptionHandling/trycatchfinally.java
+++ ExceptionHandling/trycatchfinally.java
@@ -8,10 +8,27 @@ public class trycatchfinally {
             int num = sc.nextInt();  //12.5
             System.out.println(num);
         }
+        // common exception that handles all the exception
         catch(Exception e){
             System.out.println("Exception handled");
             System.out.println(e);
         }
+
+        // catch(ArithmeticException e){
+        //     System.out.println("Arithmetic Exception Handled");
+        //     System.out.println(e);
+        // }
+
+        // catch(IndexOutOfBoundsException e){
+        //     System.out.println("Index out of bound Exception handled");
+        //     System.out.println(e);
+        // }
+
+        // catch(InputMismatchException e){
+        //     System.out.println("Input mismatch Exception handled");
+        //     System.out.println(e);
+        // }
+
         finally{
             System.out.println("Hiiii...Exception");
         }
