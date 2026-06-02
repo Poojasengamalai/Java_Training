8a826b545d3976c8ebe863db543e4c034f1cff04
Pooja s <poojasengamalai27@gmail.com>
2026-06-02 12:33:59 +0530

    Inserting at Middle in Singlly Linked List - DSA

--- DSA/LinkedList/SinglyLL.java
+++ DSA/LinkedList/SinglyLL.java
@@ -41,13 +41,88 @@ class SLL{
         head = newNode;
     }
 
+    void insertAtPosition(int val, int pos){  // insert at position
+        Node newNode = new Node();
+        newNode.data = val;
+        newNode.next = null;
+        if(pos == 1){
+            newNode.next = head;
+            head = newNode;
+        }else{
+            Node temp = head;
+            for(int i=1;i<pos-1;i++){
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+    }
+
+
+    void display(){
+        Node temp = head;
+
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    void insertAtMiddle(int val){  // insert at position
+        Node newNode = new Node();
+        newNode.data = val;
+        newNode.next = null;
+        Node temp = head;
+        int count = 0;
+        while(temp!=null){
+            count++;
+            temp = temp.next;
+        }
+        temp = head;
+        for(int i=1;i<(count/2);i++){  // this is for insert at middle and (count/2)-1 for before the middle element
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        
+    }
+
 }
 
 public class SinglyLL {
     public static void main(String[] args) {
         SLL s1 = new SLL();
+
+        // Initial values
+        s1.insertAtEnd(100);
+        s1.insertAtEnd(200);
         s1.insertAtEnd(50);
-        
+
+        System.out.println("Original List: ");
+        s1.display();
+
+        // Insert at beginning
+        s1.insertAtBegin(90);
+        System.out.println("Insert at Begining: ");
+        s1.display();
+
+        // Insert at end
+        s1.insertAtEnd(80);
+        System.out.println("Insert at End: ");
+        s1.display();
+
+        // Insert 30 at position 3
+        s1.insertAtPosition(30, 3);
+        System.out.println("Insert at Position: ");
+        s1.display();
+
+        System.out.print("After Insertions: ");
+        s1.display();
+
+        s1.insertAtMiddle(55);
+        System.out.println("Insert at Middle: ");
+        s1.display();
     }
     
 }
