d6606ae9f42d0519de07072260c0bccfc0871f3a
Pooja s <poojasengamalai27@gmail.com>
2026-06-02 11:23:58 +0530

    Inserting at end in Singlly Linked List - DSA

---
+++ DSA/LinkedList/SinglyLL.java
@@ -0,0 +1,47 @@
+package DSA.LinkedList;
+
+// Structure of the Node
+class Node{
+    int data;
+    Node next;
+}
+
+// Singlly linked list
+class SLL{
+    Node head;
+
+    // while creating node head should be null
+    SLL(){
+        head = null;
+    }
+
+    
+    void insertAtEnd(int val){    //insert at end or last
+        Node newNode = new Node();
+        newNode.data = val;
+        newNode.next = null;
+
+        if(head == null){
+            head = newNode;
+        }else{
+            Node temp = head;
+            while(temp.next!=null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+
+
+
+}
+
+public class SinglyLL {
+    public static void main(String[] args) {
+        SLL s1 = new SLL();
+        s1.insertAtEnd(50);
+        
+    }
+    
+}
