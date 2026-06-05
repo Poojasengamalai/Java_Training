b27414100bf2cd8957a34db8095cfa02c3b7952f
Pooja s <poojasengamalai27@gmail.com>
2026-06-05 12:36:48 +0530

    Graph using Adjacency List -DSA

---
+++ DSA/Graph/GraphUsingAdjacencyList.java
@@ -0,0 +1,14 @@
+package DSA.Graph;
+
+
+
+    graph(int vertices){
+        numVertices = vertices;
+        adjList = new Node_g[vertices];
+        for(int i=0; i<vertices; i++){
+            System.out.print(adjList[]+" ");
+        }
+    }
+public class GraphUsingAdjacencyList {
+    
+}
